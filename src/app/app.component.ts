707bd461beb465791a6f5acd4f31d2e74fe00132
Rajas Vyas <rajas.vyas@gmail.com>
2024-03-27 13:47:01 +0530

    Navigating the menu using *ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  
+  menu:string = "Recipes";
+
+  onMenuSelected(selectedMenu: string) {
+    this.menu = selectedMenu;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
     selector: "app-header",
@@ -6,4 +6,18 @@ import { Component } from "@angular/core";
 })
 export class HeaderComponent {
     collapsed = true;
+    @Output() menuSelected = new EventEmitter<string>();
+
+    // onRecipesClick() {
+    //     this.menuSelected.emit('Recipes');
+    // }
+    // onShoppingListClick() {
+    //     this.menuSelected.emit('ShoppingList');
+    // }
+    /** Using a single function instead of the above to commented functions */
+    onMenuClick(selectedMenu: string) {
+        this.menuSelected.emit(selectedMenu);
+    }
+
+
 }
