e66a0c474e111581e4a36ebda0e6edb4ece96504
Rajas Vyas <rajas.vyas@gmail.com>
2024-04-01 16:22:30 +0530

    Using services

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,8 +1,16 @@
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
-    recipes: Recipe[] = [
-        new Recipe('Puran Poli', "Maharashtrian dish", "https://foodtrails25.com/wp-content/uploads/2023/09/Puran-Poli-Recipe.jpg"),
-        new Recipe('Amrakhanda', "Delicous dessert made from mango pulp & curd", "https://www.nestleprofessional.in/sites/default/files/2021-08/Amrakhand.jpg")
-      ];
+  private recipes: Recipe[] = [
+    new Recipe('Puran Poli', "Maharashtrian dish", "https://foodtrails25.com/wp-content/uploads/2023/09/Puran-Poli-Recipe.jpg"),
+    new Recipe('Amrakhanda', "Delicous dessert made from mango pulp & curd", "https://www.nestleprofessional.in/sites/default/files/2021-08/Amrakhand.jpg")
+  ];
+
+  getRecipes () {
+    /**
+     * Returing only this recipe will return the reference of the array
+     * this.recipes.slice() will return a new array which will be copy & not reference of the recipes array
+     */
+    return this.recipes.slice();
+  } 
 }
