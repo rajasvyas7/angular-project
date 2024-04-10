b2b055a171f82e924ffa1280a4727269de2d291a
Rajas Vyas <rajas.vyas@gmail.com>
2024-04-11 00:28:38 +0530

    Using routes instead of *ngif to show receipes & shopping-list components

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { RouterModule, Routes } from "@angular/router";
+import { RecipesComponent } from "./recipes/recipes.component";
+import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+import { NgModule } from "@angular/core";
+
+const appRoutes: Routes = [
+    {path: '', redirectTo: '/recipes', pathMatch: 'full'},
+    {path: 'recipes', component: RecipesComponent},
+    {path: 'shoppingList', component: ShoppingListComponent}
+]; 
+
+@NgModule({
+    imports: [RouterModule.forRoot(appRoutes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
     selector: "app-header",
@@ -6,8 +7,9 @@ import { Component, EventEmitter, Output } from "@angular/core";
 })
 export class HeaderComponent {
     collapsed = true;
-    @Output() menuSelected = new EventEmitter<string>();
+    // @Output() menuSelected = new EventEmitter<string>();
 
+    constructor(private router: Router) {}
     // onRecipesClick() {
     //     this.menuSelected.emit('Recipes');
     // }
@@ -15,9 +17,10 @@ export class HeaderComponent {
     //     this.menuSelected.emit('ShoppingList');
     // }
     /** Using a single function instead of the above to commented functions */
-    onMenuClick(selectedMenu: string) {
-        this.menuSelected.emit(selectedMenu);
-    }
+    // onMenuClick(selectedMenu: string) {
+    //     // this.menuSelected.emit(selectedMenu);
+    //     this.router.navigate([`/${selectedMenu}`]);
+    // }
 
 
 }
